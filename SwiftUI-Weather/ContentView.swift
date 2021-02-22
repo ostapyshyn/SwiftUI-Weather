f08ef91fb7c1b7f102f09c10eceb2aabbb77e28f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-02-22 22:37:43 +0200

    Finish Weather App

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,35 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("LightBlue")]),
-                           startPoint: .top,
-                           endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            BackgroundView(isNight: $isNight)
             VStack {
-                Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing: 8) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium, design: .default))
-                        .foregroundColor(.white)
-                        
-                    
-                }
-                .padding(.bottom, 40)
+                CityTextView(cityName: "Cupertino, CA")
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
                 HStack(spacing: 25) {
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
                     WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
                     WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 55)
                     WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
                     WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 25)
+                }
+                Spacer()
+                Button {
+                    isNight.toggle()
+                    print(isNight ? "black" : "blue")
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
                 }
                 Spacer()
             }
@@ -67,9 +61,56 @@ struct WeatherDayView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
-            Text("\(temperature)")
+            Text("\(temperature)°")
                 .font(.system(size: 28, weight: .medium, design: .default))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .gray : Color("LightBlue")]),
+                       startPoint: .top,
+                       endPoint: .bottom).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    var body: some View {
+        
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            
+        }
+        .padding(.bottom, 40)
     }
 }
--- SwiftUI-Weather/SwiftUI_WeatherApp.swift
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -15,3 +15,4 @@ struct SwiftUI_WeatherApp: App {
         }
     }
 }
+
